86767fb4029fe413346f6f64de08d119ea889365
RuslanAgishev <agishev_ruslan@mail.ru>
2020-03-30 16:01:14 +0300

    2d path planning: Voronoi, A*, graphs

--- Path_Planning/grid.py
+++ Path_Planning/grid.py
@@ -1,4 +1,6 @@
 import numpy as np
+from bresenham import bresenham
+from scipy.spatial import Voronoi, voronoi_plot_2d
 
 
 def create_grid(data, drone_altitude, safety_distance):
@@ -36,3 +38,82 @@ def create_grid(data, drone_altitude, safety_distance):
             grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
 
     return grid
+
+
+
+
+def create_grid_and_edges(data, drone_altitude, safety_distance):
+    """
+    Returns a grid representation of a 2D configuration space
+    along with Voronoi graph edges given obstacle data and the
+    drone's altitude.
+    """
+    
+    def check_collision(p1, p2, grid):
+        line = list(bresenham(int(p1[0]),int(p1[1]), int(p2[0]),int(p2[1])))
+        for p in line:
+            # check if line is inside the grid dimensions
+            if np.abs(p[0]) < grid.shape[0] and np.abs(p[1]) < grid.shape[1]:
+                # check if line point is crossing an obstacle
+                if grid[p] == 1:
+                    return True
+        return False
+    def inside_map(p, grid):
+        return p[0]>=0 and p[0]<grid.shape[0] and p[1]>=0 and p[1]<grid.shape[1]
+
+    # minimum and maximum north coordinates
+    north_min = np.floor(np.min(data[:, 0] - data[:, 3]))
+    north_max = np.ceil(np.max(data[:, 0] + data[:, 3]))
+
+    # minimum and maximum east coordinates
+    east_min = np.floor(np.min(data[:, 1] - data[:, 4]))
+    east_max = np.ceil(np.max(data[:, 1] + data[:, 4]))
+
+    # given the minimum and maximum coordinates we can
+    # calculate the size of the grid.
+    north_size = int(np.ceil((north_max - north_min)))
+    east_size = int(np.ceil((east_max - east_min)))
+
+    # Initialize an empty grid
+    grid = np.zeros((north_size, east_size))
+    # Center offset for grid
+    north_min_center = np.min(data[:, 0])
+    east_min_center = np.min(data[:, 1])
+    
+    # Define a list to hold Voronoi points
+    points = []
+    # Populate the grid with obstacles
+    for i in range(data.shape[0]):
+        north, east, alt, d_north, d_east, d_alt = data[i, :]
+
+        if alt + d_alt + safety_distance > drone_altitude:
+            obstacle = [
+                int(north - d_north - safety_distance - north_min_center),
+                int(north + d_north + safety_distance - north_min_center),
+                int(east - d_east - safety_distance - east_min_center),
+                int(east + d_east + safety_distance - east_min_center),
+            ]
+            grid[obstacle[0]:obstacle[1]+1, obstacle[2]:obstacle[3]+1] = 1
+            
+            # add center of obstacles to points list
+            points.append([north - north_min, east - east_min])
+
+    # TODO: create a voronoi graph based on
+    # location of obstacle centres
+    graph = Voronoi(points)
+
+    # TODO: check each edge from graph.ridge_vertices for collision
+    edges = []
+    for v in graph.ridge_vertices:
+        p1 = graph.vertices[v[0]]
+        p2 = graph.vertices[v[1]]
+
+        # Then you can test each pair p1 and p2 for collision using Bresenham
+        # (need to convert to integer if using prebuilt Python package)
+        # If the edge does not hit an obstacle
+        # add it to the list
+        in_collision = check_collision(p1, p2, grid)
+        if not in_collision and inside_map(p1, grid) and inside_map(p2, grid):
+            edges.append((p1, p2))
+    
+    return grid, edges
