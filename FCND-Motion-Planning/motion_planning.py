33fa979027e2d86c9e7dc4ebe1ba62e4e8b4cebe
RuslanAgishev <agishev_ruslan@mail.ru>
2020-04-02 14:31:19 +0300

    motion planning project 1st working prototype

--- FCND-Motion-Planning/motion_planning.py
+++ FCND-Motion-Planning/motion_planning.py
@@ -5,7 +5,7 @@ from enum import Enum, auto
 
 import numpy as np
 
-from planning_utils import a_star, heuristic, create_grid
+from planning_utils import a_star, heuristic, create_grid, prune_path
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
@@ -120,27 +120,33 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-        
+        lat0, lon0, alt0 = 37.792480, -122.397450, 0.
+
         # TODO: set home position to (lon0, lat0, 0)
+        self.set_home_position(longitude=lon0, latitude=lat0, altitude=alt0)
 
         # TODO: retrieve current global position
- 
+        global_home_pose = np.array([lon0, lat0, alt0])
+
         # TODO: convert to current local position using global_to_local()
+        start_ne = global_to_local(self.global_position, self.global_home)
         
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
         data = np.loadtxt('colliders.csv', delimiter=',', dtype='Float64', skiprows=2)
-        
+        start = (int(start_ne[0]), int(start_ne[1]))
+        goal = (150, 10)
+        print(start_ne, start)
         # Define a grid for a particular altitude and safety margin around obstacles
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
+        grid_start = (start[0]-north_offset, start[1]-east_offset)
         # TODO: convert start position to current position rather than map center
         
         # Set goal as some arbitrary position on the grid
-        grid_goal = (-north_offset + 10, -east_offset + 10)
+        grid_goal = (goal[0]-north_offset, goal[1]-east_offset)
         # TODO: adapt to set goal as latitude / longitude position and convert
 
         # Run A* to find a path from start to goal
@@ -150,6 +156,8 @@ class MotionPlanning(Drone):
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
         # TODO (if you're feeling ambitious): Try a different approach altogether!
+        path = prune_path(path, 0.5)
+
 
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
